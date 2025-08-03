0ad1e4dc3765a78fdd35f386c55ffd4ab69bde0b
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2025-08-03 13:26:12 +0000

    Start!

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+if (
+    DeviceMotionEvent &&
+    typeof DeviceMotionEvent.requestPermission === "function"
+) {
+    DeviceMotionEvent.requestPermission();
+}
+
+var accelerometer_x = 0;
+var accelerometer_y = 0;
+var accelerometer_z = 0;
+var orientation_x = 0;
+var orientation_y = 0;
+var orientation_z = 0;
+
+window.addEventListener("devicemotion", updateMotion);
+window.addEventListener("deviceorientation", updateOrientation);
+
+function updateMotion(event) {
+    accelerometer_x = event.acceleration.x;
+    document.getElementById("Accelerometer_x").innerHTML = accelerometer_x;
+}
+
+function updateOrientation(event) {
+    orientation_a = event.alpha;
+    document.getElementById("Orientation_a").innerHTML = orientation_a;
+}
+
 
