e152553dd11d322e7fc0e0e358b9f68d3df5871b
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2025-08-04 19:59:25 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,8 +3,11 @@ export default {
     const url = new URL(request.url);
     
     // Let Cloudflare handle static assets automatically
-    if (['/index.html', '/', '/script.js', '/style.css'].includes(url.pathname)) {
+    if (['/index.html', '/script.js', '/style.css'].includes(url.pathname)) {
       return env.ASSETS.fetch(request);
+    } 
+    if (url.pathname === '/') {
+      return env.ASSETS.fetch("main.html")
     }
     
     return new Response("Not found", { status: 404 });
