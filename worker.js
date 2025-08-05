a34ea4425021ff8e8a229ea79a7801a01c2dfae4
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2025-08-05 22:45:20 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,12 +1,27 @@
+const SDP_CODE_LENGTH = 3;
 var worker_default = {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     if (url.pathname.startsWith("/api/")) {
-      const body = await request.json();
-      if("SDP_OFFER" in body){
-        const CODE = crypto.randomUUID().substring(0,3)
-        await write_sdp_offer(body, env, CODE);
-        return new Response(CODE);
+      if(request.method=="PUT"){
+        if(request.headers.has("SDP_CODE")){
+          const body = await request.json();
+          const CODE = request.headers.get("SDP_CODE");
+          await write_sdp_offer(body, env, CODE);
+          return new Response("Ok");
+        } else {
+          const body = await request.json();
+          if ("SDP_OFFER" in body) {
+            const CODE = crypto.randomUUID().substring(0, SDP_CODE_LENGTH);
+            //TODO check for code clash
+            await write_sdp_offer(body, env, CODE);
+            return new Response(CODE);
+          }
+        }
+      } else if(request.method == "GET"){
+        const SDP_CODE = request.headers.get("SDP_CODE");
+        const data = await env.WEB_RTC_CACHE.get(SDP_CODE);
+        return new Response(data);
       }
     }
     if (["/main.html", "/index.js", "/style.css"].includes(url.pathname)) {
